c33c22fbb6f6e5a4e03682c8d1297af148654af3
Max Arnold <max.arnold@epsilon.com>
2024-05-10 12:31:15 -0500

    upgrade to angular 17 and first parts of nav

--- QuestLife/src/app/app-routing.module.ts
+++ QuestLife/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TrackerComponent } from './tracker/tracker.component';
+import { OverviewComponent } from './overview/overview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TrackerComponent },
+  { path: 'overview', component: OverviewComponent },
+  { path: 'tracker', component: TrackerComponent },
+  { path: '**', component: TrackerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- QuestLife/src/app/app.component.ts
+++ QuestLife/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'QuestLife';
+  user = 'User'
+  otherTheme = 'dark'
 }
