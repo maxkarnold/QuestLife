c28054fd5faf03cb5c657e62465e99bd9b0fe780
Max Arnold <maxkoarnold@gmail.com>
2025-03-25 16:58:57 -0500

    updating to angular 18

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # QuestLife
 
+# SETUP
+1. Go to Firebase Console to grab firebase config: [Link](https://console.firebase.google.com/u/0/project/quest-life-44c79/settings/general/web:ODRjMzlmN2UtODM0Yy00ODAzLWJiN2QtNTI0ZDZiYzUzOWY2?fb_gclid=Cj0KCQjwqIm_BhDnARIsAKBYcmtNuAaRH_-rTxmVn9KOljMs07CztaobCdfmxLIDPAbzpnDT5QMKHBAaAtrzEALw_wcB)
+2. Create `src/environments/` folder with `environment.ts` and `environment.prod.ts` files
+3. Something like this for `environment.ts`
+```typescript
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "XXXXXXXXXXX",
+    authDomain: "XXXXXXXX",
+    projectId: "XXXXXXXX",
+    storageBucket: "XXXXXXX",
+    messagingSenderId: "XXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXXXXX"
+  }
+}
+```
+4. For `environment.prod.ts` just replace `false` with `true`
+5. `npm i` command should install all node modules
+6. `npm start` should allow for development
+7. `npm run deploy` should allow for deployment to LIVE
+
 # TODO
 ### Sections
 1. Tracker: Main page to update current xp
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { firebaseConfig } from '../environments/environment'
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 import { AttributeService } from './attribute.service';
 import { OverviewComponent } from './overview/overview.component';
 import { TrackerComponent } from './tracker/tracker.component';
@@ -19,10 +20,13 @@ import { TrackerComponent } from './tracker/tracker.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideFirestore(() => getFirestore()),
   ],
-  providers: [AttributeService],
+  providers: [
+    AttributeService,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
