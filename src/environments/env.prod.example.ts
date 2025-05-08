d5ba75500b5c70f81082730b539d983948cfd77a
Max Arnold <maxarnold@Maxs-MacBook-Pro.local>
2025-05-08 16:44:17 -0500

    update setup instructions

---
+++ src/environments/env.prod.example.ts
@@ -0,0 +1,14 @@
+// 1. Go to Firebase Console to copy firebase config: [Link](https://console.firebase.google.com/u/0/project/quest-life-44c79/settings/general/web:ODRjMzlmN2UtODM0Yy00ODAzLWJiN2QtNTI0ZDZiYzUzOWY2?fb_gclid=Cj0KCQjwqIm_BhDnARIsAKBYcmtNuAaRH_-rTxmVn9KOljMs07CztaobCdfmxLIDPAbzpnDT5QMKHBAaAtrzEALw_wcB)
+// 2. Replace firebaseConfig here with copied config
+
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "XXXXXXXXXXX",
+    authDomain: "XXXXXXXX",
+    projectId: "XXXXXXXX",
+    storageBucket: "XXXXXXX",
+    messagingSenderId: "XXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXXXXX"
+  }
+}
